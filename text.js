63b6dd2cfc23cad8fb831ea52fdcc2a7f5fac5b6
Vlad21islav <135822561+Vlad21islav@users.noreply.github.com>
2024-10-05 13:41:01 +0300

    now text is in the other file

---
+++ text.js
@@ -0,0 +1,20 @@
+const word = `
+    1.Тропический шторм - tropical storm,
+    2.объявляем чрезвычайное положение - declare a state of emergency,
+    3.В эпицентре шторма - the eye of the storm,
+    4.be под угрозой со стороны - be under threat from,
+    5. ниже уровня моря - below sea level,
+    6. во временных убежищах - in temporary shelters,
+    7.уровень воды поднимается - water rise,
+    8.просим о помощи - beg for help,
+    9.пытаемся справиться - struggle to cope,
+    10. откачиваем воду - pump water out,
+    11. теряют свои жизни - lose their lives,
+    12. медленно восстанавливаются - make a slow recovery,
+    13.дают образование - educational,
+    14. заставляют задуматься - thought-provoking,
+    15.предсказуемы - predictable,
+    16. пустая трата времени - a waste of time
+    `
+
+export default word

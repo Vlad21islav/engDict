a1168ebe9cdbd40b1e2f57bd24674619c97f8fcf
Vlad21islav <135822561+Vlad21islav@users.noreply.github.com>
2024-10-05 14:59:59 +0300

    bugs

--- script.js
+++ script.js
@@ -3,9 +3,14 @@ import word from './text.js'
 window.onload = function() {
     let words = word
     let otv = ''
+    let first_enter = true
     for (let i = 0; i <= words.length - 1; i++) {
         if (words[i] == '\n') {
-            otv += '<br/>'
+            if (!first_enter) {
+                otv += '<br/>'
+            } else {
+                first_enter = false
+            }
         } else {
             otv += words[i]
         }

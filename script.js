e1bfbe6093d4d109ff4d2ab5d9bf18e512183341
Vlad21islav <135822561+Vlad21islav@users.noreply.github.com>
2024-10-11 19:26:40 +0300

    font-size

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 import word from './text.js'
 
-window.onload = function() {
-    let words = word
-    let otv = ''
-    let first_enter = true
-    for (let i = 0; i <= words.length - 1; i++) {
-        if (words[i] == '\n') {
-            if (!first_enter) {
-                otv += '<br/>'
-            } else {
-                first_enter = false
-            }
+let words = word
+let otv = ''
+let first_enter = true
+for (let i = 0; i <= words.length - 1; i++) {
+    if (words[i] == '\n') {
+        if (!first_enter) {
+            otv += '<br/>'
         } else {
-            otv += words[i]
+            first_enter = false
         }
+    } else {
+        otv += words[i]
     }
-    document.getElementById('block').innerHTML = otv;
-};
+}
+document.getElementById('block').innerHTML = otv;
+let width = document.body.clientWidth
+document.getElementById('block').style.fontSize = `${width / 700}rem`
+
+
+addEventListener('resize', (event) => {
+    let width = document.body.clientWidth
+    document.getElementById('block').style.fontSize = `${width / 700}rem`
+})

472710b8931250873ec44a6a555d32107bd26906
Vlad21islav <135822561+Vlad21islav@users.noreply.github.com>
2024-10-11 18:06:02 +0300

    new words

--- text.js
+++ text.js
@@ -1,20 +1,20 @@
 const word = `
-    1.Тропический шторм - tropical storm
-    2.объявляем чрезвычайное положение - declare a state of emergency
-    3.В эпицентре шторма - the eye of the storm
-    4.be под угрозой со стороны - be under threat from
-    5. ниже уровня моря - below sea level
-    6. во временных убежищах - in temporary shelters
-    7.уровень воды поднимается - water rise
-    8.просим о помощи - beg for help
-    9.пытаемся справиться - struggle to cope
-    10. откачиваем воду - pump water out
-    11. теряют свои жизни - lose their lives
-    12. медленно восстанавливаются - make a slow recovery
-    13.дают образование - educational
-    14. заставляют задуматься - thought-provoking
-    15.предсказуемы - predictable
-    16. пустая трата времени - a waste of time
+    1. Погодные явления - weather phenomena
+    2. Странное явление - a weirdphenomenon
+    3. Написать мировые новости - make world news
+    4. Восстановить озоновый слой - restore the ozone layer
+    5. Облака постоянно врезаются друг в друга - clouds constantly crash into each other
+    6. Вызывают сильные грозы - cause violent lightning storms
+    7. Очевидец - a witness
+    8. Изумленный - astonished
+    9. Происходят - occour
+    10. Льет как из ведра - raning cats and dogs
+    11. Нет худа без добра - every cloud has a silver lining
+    12. Друг хорошей погоды - a fair-weather friend
+    13. Быть в тумане - to be in a fog
+    14. Неважно себя чувствовать - to be under the weather
+    15. Прогноз погоды - a weather forecast
+    16. Лесной пожар - a wildfire
     `
 
 export default word

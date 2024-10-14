84dfda9f754ef078683f815bcb5fa4202b738072
Vlad21islav <135822561+Vlad21islav@users.noreply.github.com>
2024-10-14 14:20:01 +0300

    Update text.js

--- text.js
+++ text.js
@@ -7,7 +7,7 @@ const word = `
     6. Вызывают сильные грозы - cause violent lightning storms
     7. Очевидец - a witness
     8. Изумленный - astonished
-    9. Происходят - occour
+    9. Происходят - occur
     10. Льет как из ведра - raining cats and dogs
     11. Нет худа без добра - every cloud has a silver lining
     12. Друг хорошей погоды - a fair-weather friend
